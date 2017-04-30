04e74549d4c79b3264a9a7594b70c8d345391ae8
mlcaamano <matias_caamano2004@yahoo.com.ar>
2017-04-30 01:58:52 -0300

    pantalla menu con desplegable

--- app/src/main/java/com/example/matias/lav5tp/PKG_MenuActivity/MenuActivity.java
+++ app/src/main/java/com/example/matias/lav5tp/PKG_MenuActivity/MenuActivity.java
@@ -2,6 +2,8 @@ package com.example.matias.lav5tp.PKG_MenuActivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.matias.lav5tp.R;
 
@@ -11,5 +13,21 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.desplegablemenuactivity,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
